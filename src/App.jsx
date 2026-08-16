bdeb3ea4ff363ccf485b6be2f3e5d2ccbe820e11
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-16 08:50:47 +0300

    add import CartProvider and wrap CartProvider in the app function

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext'; 
 import Navbar from './components/layout/Navbar';
 import Products from './pages/Products';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      
-      <main className="container mx-auto">
-        <Routes>
-          <Route path="/" element={<div className="p-8">Welcome to the Home Page!</div>} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/login" element={<div className="p-8">Login Page</div>} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Navbar />
+        <main className="container mx-auto">
+          <Routes>
+            <Route path="/" element={<div className="p-8">Welcome to the Home Page!</div>} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/login" element={<div className="p-8">Login Page coming soon...</div>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
