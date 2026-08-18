40b617ad0f631777591232881ed1bd0c877e0539
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-18 11:58:57 +0300

    update fix the routing of the Register.jsx from login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <Route path="/products" element={<Products />} />
             <Route path="/login" element={<Login/>} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/Register" element={<Register />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </main>
       </BrowserRouter>
