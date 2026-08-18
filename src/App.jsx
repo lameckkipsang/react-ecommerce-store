f61fe4b8f9aaaef132ca367b143d8a1831b1f24e
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-18 12:08:11 +0300

    update app.jsx to accommodate the ProtectedRoute with cart

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Cart from './pages/Cart';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import ProtectedRoute from './components/layout/ProtectedRoute';
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
             <Route path="/home" element={<Home/>} />
             <Route path="/products" element={<Products />} />
             <Route path="/login" element={<Login/>} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
             <Route path="/register" element={<Register />} />
           </Routes>
         </main>
